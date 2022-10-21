4e5be87bdf3bb7f1cde87f79bc09d15b9370d7f2
Dzulfikri19 <114043266+Dzulfikri19@users.noreply.github.com>
2022-10-21 10:07:40 +0700

    Add files via upload

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+
+	public Text txScore;
+	public Text txHighScore;
+	Text txSelamat;
+	int highscore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		highscore = PlayerPrefs.GetInt("HS", 0);
+		if (Data.score > highscore){
+			highscore = Data.score;
+			PlayerPrefs.SetInt("HS", highscore);
+		} else if (EnemyController.EnemyKilled == 3){
+			SceneManager.LoadScene("Congratulation");
+		}
+		txHighScore.text = "Highscores : "+ highscore;
+		txScore.text = "Scores : "+ Data.score;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void Replay(){
+		Data.score = 0;
+		EnemyController.EnemyKilled = 0;
+		SceneManager.LoadScene("Gameplay");
+	}
+}
